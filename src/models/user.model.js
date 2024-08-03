8387ab4aec1356f7f975725d7c02526866d8289e
Mihir Bavisi <mihirbavishi111@gmail.com>
2024-08-03 02:44:37 +0200

    Add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,77 @@
+import jsonwebtoken from 'jsonwebtoken'
+import mongoose, { Schema } from 'mongoose'
+import bcrypt from 'bcrypt'
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, // cloudinary url
+        required: true,
+    },
+    coverImage: {
+        type: String, // cloudinary url
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Video'
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    refreshToken: {
+        type: String,
+    },
+}, { timestamps: true })
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next()
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+userSchema.methods.generateToken = function () {
+    return jsonwebtoken.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: '1h' })
+}
+userSchema.methods.generateAccessToken = function () {
+    return jsonwebtoken.sign({
+        id: this._id,
+        username: this.username,
+        email: this.email,
+        fullname: this.fullname,
+    }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    },
+    )
+}
+userSchema.methods.generateRefreshToken = function () {
+    return jsonwebtoken.sign({
+        id: this._id,
+    }
+    )
+}
+export const User = mongoose.model("User", userSchema)
